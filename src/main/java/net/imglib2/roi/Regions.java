364dd5fbb44bee84daa157232bb23c03445cd619
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-01-24 23:02:53 +0100

    Regions.sample(region, RA<T>) binds region to img, giving an Iterable<T> over those pixels of the RA that are covered by the region.

--- src/main/java/net/imglib2/roi/Regions.java
+++ src/main/java/net/imglib2/roi/Regions.java
@@ -1,7 +1,10 @@
 package net.imglib2.roi;
 
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.roi.util.IterableRandomAccessibleRegion;
+import net.imglib2.roi.util.SamplingIterableInterval;
 import net.imglib2.type.BooleanType;
 
 public class Regions
@@ -9,6 +12,11 @@ public class Regions
 	// TODO: make Positionable and Localizable
 	// TODO: bind to (respectively sample from) RandomAccessible
 	// TODO: out-of-bounds / clipping
+
+	public static < B extends BooleanType< B >, T > IterableInterval< T > sample( final IterableInterval< B > region, final RandomAccessible< T > img )
+	{
+		return SamplingIterableInterval.create( region, img );
+	}
 
 	@SuppressWarnings( "unchecked" )
 	public static < B extends BooleanType< B > > IterableRegion< B > iterable( final RandomAccessibleInterval< B > region )
