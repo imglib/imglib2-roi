becb64dfad81ee01e1216599b3a39143eedef06e
dietzc <dietzc85@googlemail.com>
2015-09-12 16:59:15 -0500

    adds bresenham to ROIUtils to interpolate between 2d RealLocalizables

--- src/main/java/net/imglib2/roi/util/ROIUtils.java
+++ src/main/java/net/imglib2/roi/util/ROIUtils.java
@@ -33,6 +33,7 @@
  */
 package net.imglib2.roi.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -117,5 +118,51 @@ public class ROIUtils
 		}
 
 		return new FinalRealInterval( min, max );
+	}
+
+	public static List< Localizable > bresenham( final List< ? extends RealLocalizable > vertices )
+	{
+
+		assert( vertices.size() > 1 );
+		assert( vertices.iterator().next().numDimensions() == 2 );
+
+		final ArrayList< Localizable > tmp = new ArrayList< Localizable >();
+		for ( int i = 0; i < vertices.size(); i++ )
+		{
+
+			long x0 = Math.round( vertices.get( i ).getDoublePosition( 0 ) );
+			long y0 = Math.round( vertices.get( i ).getDoublePosition( 1 ) );
+			final long x1 = Math.round( vertices.get( ( i + 1 ) % vertices.size() ).getDoublePosition( 0 ) );
+			final long y1 = Math.round( vertices.get( ( i + 1 ) % vertices.size() ).getDoublePosition( 1 ) );
+
+			final long dx = Math.abs( x1 - x0 ), sx = x0 < x1 ? 1 : -1;
+			final long dy = -Math.abs( y1 - y0 ), sy = y0 < y1 ? 1 : -1;
+
+			long err = dx + dy, e2; /* error value e_xy */
+
+			while ( true )
+			{
+				tmp.add( new Point( x0, y0 ) );
+				if ( x0 == x1 && y0 == y1 )
+					break;
+				e2 = 2 * err;
+				if ( e2 > dy )
+				{
+					err += dy;
+					x0 += sx;
+				} /* e_xy+e_x > 0 */
+				if ( e2 < dx )
+				{
+					err += dx;
+					y0 += sy;
+				} /* e_xy+e_y < 0 */
+			}
+
+			// remove last point, because the last point is the identical to the
+			// first point of the next edge
+			tmp.remove( tmp.size() - 1 );
+		}
+
+		return tmp;
 	}
 }
