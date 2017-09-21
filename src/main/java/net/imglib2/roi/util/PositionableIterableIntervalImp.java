7647888b641821ea0c072cde3300c3c670588402
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-09-21 13:07:07 +0200

    Simplify PositionableIterableIntervalImp
    probably can be removed soon

--- src/main/java/net/imglib2/roi/util/PositionableIterableIntervalImp.java
+++ src/main/java/net/imglib2/roi/util/PositionableIterableIntervalImp.java
@@ -33,9 +33,6 @@
  */
 package net.imglib2.roi.util;
 
-import java.util.Iterator;
-
-import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
 import net.imglib2.roi.PositionableIterableInterval;
 
@@ -53,110 +50,14 @@ public class PositionableIterableIntervalImp< T, I extends IterableInterval< T >
 		extends AbstractPositionableIterableInterval< T, I, PositionableIterableIntervalImp< T, I > >
 		implements PositionableIterableInterval< T, PositionableIterableIntervalImp< T, I > >
 {
-	protected final I source;
-
 	public PositionableIterableIntervalImp( final I source )
 	{
 		super( source );
-		this.source = source;
 	}
 
 	protected PositionableIterableIntervalImp( final PositionableIterableIntervalImp< T, I > other )
 	{
 		super( other );
-		this.source = other.source;
-	}
-
-	@Override
-	public long size()
-	{
-		return source.size();
-	}
-
-	@Override
-	public T firstElement()
-	{
-		return source.firstElement();
-	}
-
-	@Override
-	public Object iterationOrder()
-	{
-		return this;
-	}
-
-	@Override
-	public Iterator< T > iterator()
-	{
-		return cursor();
-	}
-
-	@Override
-	public Cursor< T > cursor()
-	{
-		return new PositionableIterableIntervalCursor( source.cursor() );
-	}
-
-	@Override
-	public Cursor< T > localizingCursor()
-	{
-		return new PositionableIterableIntervalCursor( source.localizingCursor() );
-	}
-
-	class PositionableIterableIntervalCursor extends OffsetLocalizable< Cursor< T > > implements Cursor< T >
-	{
-		public PositionableIterableIntervalCursor( final Cursor< T > cursor )
-		{
-			super( cursor, currentOffset );
-		}
-
-		@Override
-		public T get()
-		{
-			return null;
-		}
-
-		@Override
-		public void jumpFwd( final long steps )
-		{
-			source.jumpFwd( steps );
-		}
-
-		@Override
-		public void fwd()
-		{
-			source.fwd();
-		}
-
-		@Override
-		public void reset()
-		{
-			source.reset();
-		}
-
-		@Override
-		public boolean hasNext()
-		{
-			return source.hasNext();
-		}
-
-		@Override
-		public T next()
-		{
-			return source.next();
-		}
-
-		@Override
-		public PositionableIterableIntervalCursor copy()
-		{
-			return new PositionableIterableIntervalCursor( source.copyCursor() );
-		}
-
-		@Override
-		public PositionableIterableIntervalCursor copyCursor()
-		{
-			return copy();
-		}
 	}
 
 	@Override
