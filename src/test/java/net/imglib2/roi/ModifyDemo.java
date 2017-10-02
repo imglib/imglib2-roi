aa73ec43e8c6fb7b320e8f412b817be717e8ef76
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-10-02 02:06:42 +0200

    Add demo of propagating mask changes to composite masks

---
+++ src/test/java/net/imglib2/roi/ModifyDemo.java
@@ -0,0 +1,161 @@
+package net.imglib2.roi;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Arrays;
+
+import net.imglib2.RealPoint;
+import net.imglib2.RealRandomAccessibleRealInterval;
+import net.imglib2.realtransform.AffineTransform2D;
+import net.imglib2.roi.Bounds.SmallestContainingInterval;
+import net.imglib2.roi.geom.real.ClosedWritableSphere;
+import net.imglib2.roi.geom.real.WritableSphere;
+import net.imglib2.type.logic.BoolType;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.LinAlgHelpers;
+
+import org.scijava.ui.behaviour.DragBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.io.InputTriggerDescription;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+import bdv.util.AxisOrder;
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.util.BdvOverlay;
+import bdv.util.BdvSource;
+import bdv.viewer.ViewerPanel;
+import bdv.viewer.animate.TextOverlayAnimator;
+import bdv.viewer.animate.TextOverlayAnimator.TextPosition;
+
+public class ModifyDemo
+{
+	public static void main( final String[] args )
+	{
+		// Create 3 WritableSpheres
+		final WritableSphere s1 = new ClosedWritableSphere( new double[] { 0, 0, 0 }, 3.5 );
+		final WritableSphere s2 = new ClosedWritableSphere( new double[] { 1, 2, 0 }, 1.5 );
+		final WritableSphere s3 = new ClosedWritableSphere( new double[] { 2, 2, 0 }, 1.5 );
+
+		// Combine the WritableSpheres using operators
+		final RealMaskRealInterval composite = s1.and( s2 ).and( s3 );
+
+		final InputTriggerConfig config = new InputTriggerConfig( Arrays.asList( new InputTriggerDescription( new String[] {"not mapped"}, "2d drag rotate", "bdv" ) ) );
+
+		// Wrap the intersection as a RealRandomAccessibleRealInterval
+		final RealRandomAccessibleRealInterval< BoolType > s1rrai = Masks.toRealRandomAccessibleRealInterval( s1 );
+		final BdvSource bdv1 = BdvFunctions.show(
+				s1rrai,
+				new SmallestContainingInterval( s1rrai ),
+				"Writable Sphere 1",
+				Bdv.options().is2D().axisOrder( AxisOrder.XYZ ).inputTriggerConfig( config ) );
+		final RealRandomAccessibleRealInterval< BoolType > s2rrai = Masks.toRealRandomAccessibleRealInterval( s2 );
+		final BdvSource bdv2 = BdvFunctions.show(
+				s2rrai,
+				new SmallestContainingInterval( s2rrai ),
+				"Writable Sphere 2",
+				Bdv.options().addTo( bdv1 ).axisOrder( AxisOrder.XYZ ) );
+		final RealRandomAccessibleRealInterval< BoolType > s3rrai = Masks.toRealRandomAccessibleRealInterval( s3 );
+		final BdvSource bdv3 = BdvFunctions.show(
+				s3rrai,
+				new SmallestContainingInterval( s3rrai ),
+				"Writable Sphere 3",
+				Bdv.options().addTo( bdv1 ).axisOrder( AxisOrder.XYZ ) );
+		final RealRandomAccessibleRealInterval< BoolType > compositerrai = Masks.toRealRandomAccessibleRealInterval( composite );
+		final BdvSource bdv4 = BdvFunctions.show(
+				compositerrai,
+				new SmallestContainingInterval( compositerrai ),
+				"composite",
+				Bdv.options().addTo( bdv1 ).axisOrder( AxisOrder.XYZ ) );
+
+		final ViewerPanel viewer = bdv1.getBdvHandle().getViewerPanel();
+		BdvFunctions.showOverlay( new BdvOverlay()
+		{
+			@Override
+			protected void draw( final Graphics2D g )
+			{
+				g.setColor( Color.RED );
+				if ( Intervals.isEmpty( compositerrai ) )
+					g.drawString( "composite interval is empty", 400, 300 );
+				else
+				{
+					final AffineTransform2D t = new AffineTransform2D();
+					this.getCurrentTransform2D( t );
+					final double[] min = new double[ 2 ];
+					final double[] max = new double[ 2 ];
+					t.apply( new double[] { composite.realMin( 0 ), composite.realMin( 1 ) }, min );
+					t.apply( new double[] { composite.realMax( 0 ), composite.realMax( 1 ) }, max );
+					final int x = ( int ) min[ 0 ];
+					final int y = ( int ) min[ 1 ];
+					final int width = ( int ) max[ 0 ] - x;
+					final int height = ( int ) max[ 1 ] - y;
+					g.drawRect( x, y, width, height );
+				}
+			}
+		}, "composite interval", Bdv.options().addTo( bdv1 ) );
+
+		bdv1.setColor( new ARGBType( 0x440000 ) );
+		bdv2.setColor( new ARGBType( 0x004400 ) );
+		bdv3.setColor( new ARGBType( 0x000044 ) );
+		bdv4.setColor( new ARGBType( 0xffff00 ) );
+
+		final Behaviours behaviours = new Behaviours( new InputTriggerConfig() );
+		behaviours.behaviour( new DragBehaviour()
+		{
+			WritableSphere s = null;
+
+			final double[] offset = new double[ 3 ];
+
+			@Override
+			public void init( final int x, final int y )
+			{
+				final double[] pos = getPos( x, y );
+				if ( s1.test( RealPoint.wrap( pos ) ) )
+					s = s1;
+				else if ( s2.test( RealPoint.wrap( pos ) ) )
+					s = s2;
+				else if ( s3.test( RealPoint.wrap( pos ) ) )
+					s = s3;
+				else
+					s = null;
+
+				if ( s != null )
+				{
+					final double[] t = new double[ 3 ];
+					s.center().localize( t );
+					LinAlgHelpers.subtract( t, pos, offset );
+				}
+			}
+
+			@Override
+			public void end( final int x, final int y )
+			{
+				s = null;
+			}
+
+			@Override
+			public void drag( final int x, final int y )
+			{
+				if ( s != null )
+				{
+					final double[] center = new double[ 3 ];
+					LinAlgHelpers.add( getPos( x, y ), offset, center );
+					s.center().setPosition( center );
+					viewer.requestRepaint();
+				}
+			}
+
+			double[] getPos( final int x, final int y )
+			{
+				final double[] pos = new double[ 3 ];
+				viewer.displayToGlobalCoordinates( x, y, RealPoint.wrap( pos ) );
+				return pos;
+			}
+
+		}, "move object", "button1" );
+		behaviours.install( bdv1.getBdvHandle().getTriggerbindings(), "demo" );
+
+		viewer.addOverlayAnimator( new TextOverlayAnimator( "left click + drag to move objects", 8000, TextPosition.CENTER ) );
+	}
+}
