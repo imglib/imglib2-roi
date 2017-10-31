6d230d8eda3694f73f7f1b3ba8fa0da8e62f2985
Alison Walter <awalter2@wisc.edu>
2017-10-31 10:50:23 -0500

    Add methods for retrieving empty and all Masks

--- src/main/java/net/imglib2/troi/Masks.java
+++ src/main/java/net/imglib2/troi/Masks.java
@@ -1,10 +1,19 @@
 package net.imglib2.troi;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
+import net.imglib2.FinalInterval;
+import net.imglib2.FinalRealInterval;
+import net.imglib2.Interval;
 import net.imglib2.Localizable;
+import net.imglib2.RealInterval;
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealRandomAccessibleRealInterval;
+import net.imglib2.troi.util.DefaultMask;
+import net.imglib2.troi.util.DefaultMaskInterval;
+import net.imglib2.troi.util.DefaultRealMask;
+import net.imglib2.troi.util.DefaultRealMaskRealInterval;
 import net.imglib2.troi.util.RealMaskRealIntervalAsRRARI;
 import net.imglib2.type.logic.BoolType;
 
@@ -124,5 +133,158 @@ public class Masks
 	public static RealRandomAccessibleRealInterval< BoolType > toRRARI( final RealMaskRealInterval mask )
 	{
 		return new RealMaskRealIntervalAsRRARI<>( mask, new BoolType() );
+	}
+
+	/*
+	 * Empty Masks
+	 * ===============================================================
+	 */
+
+	/**
+	 * Creates a {@link Mask} which returns {@code false} for every location.
+	 *
+	 * @param numDims
+	 *            number of dimensions the resulting Mask should have
+	 * @return {@link Mask} which returns false for all locations
+	 */
+	public static Mask emptyMask( final int numDims )
+	{
+		return new DefaultMask( numDims, BoundaryType.UNSPECIFIED, t -> false )
+		{
+			@Override
+			public boolean isEmpty()
+			{
+				return true;
+			}
+		};
+	}
+
+	/**
+	 * Creates a {@link MaskInterval} which returns {@code false} for every
+	 * location and has empty interval bounds (i.e. min &gt; max).
+	 *
+	 * @param numDims
+	 *            number of dimensions the resulting MaskInterval should have
+	 * @return {@link MaskInterval} which returns false for all locations
+	 */
+	public static MaskInterval emptyMaskInterval( final int numDims )
+	{
+		return new DefaultMaskInterval( emptyInterval( numDims ), BoundaryType.UNSPECIFIED, t -> false );
+	}
+
+	/**
+	 * Creates a {@link RealMask} which returns {@code false} for every
+	 * location.
+	 *
+	 * @param numDims
+	 *            number of dimensions the resulting RealMask should have
+	 * @return {@link RealMask} which returns false for all locations
+	 */
+	public static RealMask emptyRealMask( final int numDims )
+	{
+		return new DefaultRealMask( numDims, BoundaryType.UNSPECIFIED, t -> false )
+		{
+			@Override
+			public boolean isEmpty()
+			{
+				return true;
+			}
+		};
+	}
+
+	/**
+	 * Creates a {@link RealMaskRealInterval} which returns {@code false} for
+	 * every location and has empty interval bounds (i.e. min &gt; max).
+	 *
+	 * @param numDims
+	 *            number of dimensions the resulting RealMaskRealInterval should
+	 *            have
+	 * @return {@link RealMaskRealInterval} which returns false for all
+	 *         locations
+	 */
+	public static RealMaskRealInterval emptyRealMaskRealInterval( final int numDims )
+	{
+		return new DefaultRealMaskRealInterval( emptyRealInterval( numDims ), BoundaryType.UNSPECIFIED, t -> false );
+	}
+
+	/*
+	 * Empty Intervals
+	 * ===============================================================
+	 */
+
+	/**
+	 * Creates an {@link Interval} which has min &gt; max for all dimensions.
+	 *
+	 * @param numDims
+	 *            dimensions of the resulting {@link Interval}
+	 * @return {@link Interval} with min &gt; max
+	 */
+	public static Interval emptyInterval( final int numDims )
+	{
+		final long[] min = new long[ numDims ];
+		Arrays.fill( min, Long.MAX_VALUE );
+		final long[] max = new long[ numDims ];
+		Arrays.fill( max, Long.MIN_VALUE );
+		return new FinalInterval( min, max );
+	}
+
+	/**
+	 * Creates an {@link RealInterval} which has min &gt; max for all
+	 * dimensions.
+	 *
+	 * @param numDims
+	 *            dimensions of the resulting {@link RealInterval}
+	 * @return {@link RealInterval} with min &gt; max
+	 */
+	public static RealInterval emptyRealInterval( final int numDims )
+	{
+		final double[] min = new double[ numDims ];
+		Arrays.fill( min, Double.POSITIVE_INFINITY );
+		final double[] max = new double[ numDims ];
+		Arrays.fill( max, Double.NEGATIVE_INFINITY );
+		return new FinalRealInterval( min, max );
+	}
+
+	/*
+	 * All space Masks
+	 * ===============================================================
+	 */
+
+	/**
+	 * Creates a {@link Mask} which returns {@code true} for all locations.
+	 *
+	 * @param numDims
+	 *            number of dimensions of the {@link Mask}
+	 * @return {@link Mask} which is true for all locations
+	 */
+	public static Mask allMask( final int numDims )
+	{
+		return new DefaultMask( numDims, BoundaryType.UNSPECIFIED, t -> true )
+		{
+			@Override
+			public boolean isAll()
+			{
+				return true;
+			}
+		};
+	}
+
+	/**
+	 * Creates a {@link RealMask} which returns {@code true} for all locations.
+	 *
+	 * @param numDims
+	 *            number of dimensions of the {@link RealMask}
+	 * @return {@link RealMask} which is true for all locations
+	 */
+	public static RealMask allRealMask( final int numDims )
+	{
+		return new DefaultRealMask( numDims, BoundaryType.UNSPECIFIED, t -> true )
+		{
+			@Override
+			public boolean isAll()
+			{
+				return true;
+			}
+		};
 	}
 }
