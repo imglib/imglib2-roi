76ed17a0d14899133e6656aa317f1e33380782a0
Matthias Arzt <arzt@mpi-cbg.de>
2019-01-11 15:54:51 +0100

    Add Benchmark to compare performance of NTree agains SparseBitmask

---
+++ src/test/java/net/imglib2/roi/sparse/SparseBitmaskBenchmark.java
@@ -0,0 +1,54 @@
+package net.imglib2.roi.sparse;
+
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+import net.imglib2.algorithm.neighborhood.HyperSphereShape;
+import net.imglib2.algorithm.neighborhood.Neighborhood;
+import net.imglib2.img.sparse.NtreeImg;
+import net.imglib2.img.sparse.NtreeImgFactory;
+import net.imglib2.type.BooleanType;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.logic.NativeBoolType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+public class SparseBitmaskBenchmark
+{
+	@Benchmark
+	public void benchmark() {
+		SparseBitmask bitmask = new SparseBitmask( 2 );
+		fillHypersphere( bitmask, 50, 50, 50 );
+	}
+
+	@Benchmark
+	public void benchmarkNDTree() {
+		NtreeImg< UnsignedByteType, ? > image = new NtreeImgFactory<>( new UnsignedByteType() ).create( 100, 100 );
+		fillHypersphere( image, 50, 50, 50 );
+	}
+
+	public static void main( final String... args ) throws RunnerException
+	{
+		final Options opt = new OptionsBuilder()
+				.include( SparseBitmaskBenchmark.class.getSimpleName() )
+				.forks( 0 )
+				.warmupIterations( 4 )
+				.measurementIterations( 8 )
+				.warmupTime( TimeValue.milliseconds( 100 ) )
+				.measurementTime( TimeValue.milliseconds( 100 ) )
+				.build();
+		new Runner( opt ).run();
+	}
+
+	private static void fillHypersphere( RandomAccessible< ? extends NumericType<?> > img, long radius, long... position )
+	{
+		RandomAccess< ? extends Neighborhood< ? extends NumericType< ? > > > a = new HyperSphereShape( radius ).neighborhoodsRandomAccessible( img ).randomAccess();
+		a.setPosition( position );
+		a.get().forEach( t -> t.setOne() );
+	}
+}
